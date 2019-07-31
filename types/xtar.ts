9c4151a2768a5c234d85d37028fca900b33488d7
april <mengen.he@oneroot.io>
2019-07-31 14:16:30 +0800

    t

--- types/xtar.ts
+++ types/xtar.ts
@@ -14,7 +14,24 @@ import { XtarSlide } from './slide'
  * Please do not invoke this method directly.
  * Call `Vue.use(ElementUI)` to install.
  */
+// export function install (vue: typeof Vue, options: {}): void
+
+// export interface InstallationOptions {
+//     locale: any,
+//     i18n: any,
+//     size: string
+//   }
+  
+  /** The version of element-ui */
+//   export const version: string
+  
+  /**
+   * Install all element-ui components into Vue.
+   * Please do not invoke this method directly.
+   * Call `Vue.use(ElementUI)` to install.
+   */
 // export function install (vue: typeof Vue, options: InstallationOptions): void
+export const install = function (vue: typeof Vue, opts = {}) {};
 
 /** ElementUI component common definition */
 export type Component = XtarComponent
